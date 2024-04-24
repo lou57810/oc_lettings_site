e3d84e739f4033c96afe07d9a10dbecd12f77b34
Bernard Oswald <bernard.oswald@free.fr>
2024-04-24 23:11:46 +0200

    First commit.

---
+++ oc_lettings_site/urls.py
@@ -0,0 +1,33 @@
+"""oc_lettings_site URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+# from lettings import views
+import lettings.views
+# from profiles import views
+import profiles.views
+
+urlpatterns = [
+    path('', lettings.views.home, name='home'),
+    # path('lettings/', lettings.views.lettings_index, name='lettings_index'),
+    path('lettings/', lettings.views.index, name='lettings_index'),
+    path('profiles/<int:letting_id>/', lettings.views.letting, name='letting'),
+    # path('profiles/', profiles.views.profiles_index, name='profiles_index'),
+    path('profiles/', profiles.views.index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles.views.profile, name='profile'),
+    path('admin/', admin.site.urls),
+    
+]
