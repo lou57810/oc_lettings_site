8812784544f4c3ae1844e4380c7a785deeb6513b
lou57810 <bernard.oswald@free.fr>
2024-06-17 16:03:32 +0200

    Regul STATIC_ROOT.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -185,7 +185,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+# STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 # STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / "static", ]
