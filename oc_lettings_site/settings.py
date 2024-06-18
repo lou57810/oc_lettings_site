5d466808bd239cfe6319472fcb6f2be209f171e4
lou57810 <bernard.oswald@free.fr>
2024-06-18 16:52:34 +0200

    allowed_hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -72,7 +72,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 # ALLOWED_HOSTS = ['172.16.1.108', '127.0.0.1', 'localhost']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["*"]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'   # Ajouté pour le fonctionnement de Sentry sdk
 
 # Application definition
